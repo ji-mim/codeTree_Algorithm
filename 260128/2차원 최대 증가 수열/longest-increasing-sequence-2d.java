adc351ecfdae16b32550c57214b46f169bdcb5ef
jimin Choi <138632648+ji-mim@users.noreply.github.com>
2026-01-28 19:35:10 +0900

    Title: 2차원 최대 증가 수열, Time: 146ms, Memory: 11MB, Status: Wrong Answer - Codetree

---
+++ 260128/2차원 최대 증가 수열/longest-increasing-sequence-2d.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class Main {
+    public static int N, M; 
+    public static int [][] arr ; 
+    public static int [][] dp ;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 맨 윗줄, 맨 왼쪽 줄은 0, 기억자 반대 형태로 쭉 돌면서 dp를 돌면 될 것 같은데 
+
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        arr = new int[N][M];
+
+        for (int i = 0 ; i < N ; i ++){
+            for (int j = 0 ; j < N ; j ++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        dp = new int[N][M];
+
+        dp[0][0] = 1 ; 
+
+        for (int i = 1; i < N ; i ++){
+            for (int j = 1 ; j < N ; j ++){
+                for (int k = 0 ; k < i ; k ++){
+                    for (int l = 0 ; l < j ; l ++){
+                        if(arr[k][l] >= arr[i][j]){
+                            continue;
+                        }
+
+                        dp[i][j] = Math.max(dp[i][j], dp[k][l] + 1);
+                    }
+                }
+            }
+        }
+
+        int ans = 0 ;
+
+        for (int i = 0 ; i < N ; i ++){
+            for (int j = 0 ; j < N ; j ++){
+                ans = Math.max(ans, dp[i][j]);
+            }
+        }
+
+        System.out.println(ans);
+
+        
+    }
+}
