0636eebc07f188977a43a6c6d37498ac1634f276
jimin Choi <138632648+ji-mim@users.noreply.github.com>
2026-01-28 20:21:30 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 265ms, Memory: 14MB, Status: Wrong Answer - Codetree

---
+++ 260128/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+
+class Line implements Comparable<Line>{
+    int x;
+    int y;
+    public Line(int x, int y){
+        this.x = x; 
+        this.y = y;
+    }
+    @Override
+    public int compareTo(Line l){
+        return Integer.compare(this.x, l.x);
+    }
+}
+
+
+public class Main {
+
+    public static int N;
+    public static int[] dp;
+    public static List<Line> lines = new ArrayList<>();
+
+    public static void main(String[] args) {
+        // x좌표를 기준으로 정렬을 하고, 앞에서부터 가능한걸 선택해볼까
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        dp = new int[N];
+        for (int i = 0 ; i < N ; i ++){
+            lines.add(new Line(sc.nextInt(), sc.nextInt()));
+        }
+
+        Collections.sort(lines);
+
+        dp[0] = 1; 
+
+        for (int i = 1 ; i < N ; i ++){
+            for (int j = 0 ; j < i ; j ++){
+                if (dp[j] == 0){
+                    continue;
+                }
+
+                if(lines.get(j).y < lines.get(i).x){  // y가 x보다 작아야 함  
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        int ans = 0 ;
+        for (int i = 0 ; i < N ; i ++){
+            ans = Math.max(ans, dp[i]);
+        }
+
+        System.out.println(ans);
+
+    }
+}
