ec6fd2b4cb0ae18ea8aadba083fb66c6010cb569
jimin Choi <138632648+ji-mim@users.noreply.github.com>
2026-02-02 19:18:38 +0900

    Title: 1, 2, 5 더하기, Time: 123ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260202/1, 2, 5 더하기/1-2-5-plus.java
+++ 260202/1, 2, 5 더하기/1-2-5-plus.java
@@ -2,7 +2,32 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    public static int N;
+    public static int[] dp;
+    public static int[] nums = {1,2,5};
+
+
     public static void main(String[] args) {
-        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+
+        dp = new int[N + 1];
+
+        dp[0] = 1;
+        // dp[1] = 1 ; 
+
+        for (int i = 1; i <= N ; i ++){
+            for (int j = 0 ; j < 3 ; j ++){
+                if(i >= nums[j]){
+                    dp[i] += dp[i - nums[j]] % 10007;
+                }
+            }
+        }
+
+        System.out.println(dp[N]);
+
+
+
+
     }
 }
